e7002942cb1f5f046c8981db9868d31113a03b65
Numaan Kazi <numaankazi145@gmail.com>
2026-01-21 15:57:10 +0530

    Remove logout console log
    Remove console log from LogoutHandle function.

--- src/pages/User/Competency-Page.jsx
+++ src/pages/User/Competency-Page.jsx
@@ -120,7 +120,6 @@ export function CompetencyPage() {
   function LogoutHandle() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-    console.log("Logout");
     setLoader(true);
     setCompleted(false);
     setTimeout(() => {
